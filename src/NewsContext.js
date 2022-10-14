7e27cb6eb98bf6c5b8c9fad7a53acca4579f0ae3
FASNA-KA <jasmeeazeez813@gmail.com>
2022-10-14 13:49:54 +0530

    News-Inshots

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -1,19 +1,15 @@
 import React, { createContext, useEffect, useState } from "react";
-import axios from "axios";
 
 export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
-  const apiKey = "d3a68d3a93a54948a016a1553bc4d20c";
 
   useEffect(() => {
-    axios
-      .get(
-        `http://newsapi.org/v2/everything?q=rich&from=2020-07-19&sortBy=publishedAt&apiKey=${apiKey}`
-      )
-      .then((response) => setData(response.data))
-      .catch((error) => console.log(error));
+    fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-09-14&sortBy=publishedAt&apiKey=062a468d991343b891a8b850e88e0dc2
+      `)
+      .then((res) => res.json())
+      .then((json) => setData(json));
   }, []);
 
   return (
